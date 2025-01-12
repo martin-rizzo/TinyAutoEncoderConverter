724a5a673065a17d998ae7c5629176999b980cbf
Martin Rizzo <martinrizzo@gmail.com>
2025-01-12 18:05:13 -0300

    feat(makeall.sh): add --help option for usage information
    Added a `--help` option to the `makeall.sh` script, providing users
    with simple usage instructions.

--- makeall.sh
+++ makeall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # File    : makeall.sh
-# Purpose : Generate all models (VAEs and transcoders) from the Tiny AutoEncoder models.
+# Purpose : Build all models (VAEs and transcoders) from the Tiny AutoEncoder models.
 # Author  : Martin Rizzo | <martinrizzo@gmail.com>
 # Date    : Nov 26, 2024
 # Repo    : https://github.com/martin-rizzo/TinyModelsForLatentConversion
@@ -30,7 +30,13 @@
 #   TORT OR OTHERWISE, ARISING FROM,OUT OF OR IN CONNECTION WITH THE
 #   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
-
+HELP="
+Usage: $0 [options]
+Options:
+    -h, --help        Show this help message and exit
+    --float16 --half  Build models in float16
+    --float32         Build models in float32
+"
 MODEL_DIR='./original_taesd_models'
 ORIGINAL_MODEL='diffusion_pytorch_model.safetensors'
 
@@ -39,13 +45,15 @@ SDXL_DIR="$MODEL_DIR/taesdxl"
 SD3_DIR="$MODEL_DIR/taesd3"
 FLUX_DIR="$MODEL_DIR/taef1"
 
-
+# extra parameters to pass to the build scripts
+# (by default enable color output)
 EXTRA_PARAMS=( '--color' )
 
-# analizar uno por uno los parametros
+# loop through the arguments and set the corresponding parameters
+SHOW_HELP=false
 for arg in "$@"; do
     case $arg in
-        --half|--float16)
+        --float16|--half)
             EXTRA_PARAMS+=( '--float16' )
             shift
             ;;
@@ -53,12 +61,20 @@ for arg in "$@"; do
             EXTRA_PARAMS+=( '--float32' )
             shift
             ;;
+        -h|--help)
+            SHOW_HELP=true
+            ;;
         *)
             echo "ERROR: Unknown argument $arg"
             exit 1
             ;;
     esac
 done
+
+if [[ $SHOW_HELP == true ]]; then
+    echo "$HELP"
+    exit 0
+fi
 echo
 
 # BUILD VAEs FOR ALL MODELS
